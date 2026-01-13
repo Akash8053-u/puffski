080db591f6b9c741fffc5c1850d2af4136ae09eb
Akash <akash@gmail.com>
2026-01-13 14:57:16 +0530

    all added

--- api_puffski/app/models/index.js
+++ api_puffski/app/models/index.js
@@ -1,26 +1,5 @@
 const mongoose = require('mongoose');
-
-// Import models
-const User = require('./users');
-const Favourite = require('./Favourite');
-const UpdatedPassword = require('./UpdatedPassword');
-const Item = require('./item');
-const wishList = require('./wishList');
-const LsrProduct = require('./lsrProduct');
-const LsrCategory = require('./lsrCategory');
-const PromoCodes = require('./PromoCodes');
-const ReserveOrder = require('./ReserveOrders');
-const UserLogin = require('./userLogin');
-const UserActivity = require('./UserActivity');
-const WebsiteViewed = require('./Websiteviewed');
-const Subscribename = require('./Subscribename');
-const Faq = require('./Faq');
-const notifications = require('./Notifications');
-const Blogs = require('./Blogs');
-const Category = require('./category');
-
-<<<<<<< HEAD
-db.url= require('../config/db')
+const db = {}
 db.User=require('./users')
 db.mongoose=mongoose
 db.Favourite= require('./Favourite')
@@ -40,28 +19,10 @@ db.Blogs = require('./Blogs')
 db.Category = require('./category')
 db.Itemcategory = require('./ItemCategory')
 db.Itemproduct = require('./Itemproduct')
-module.exports= db
-=======
-const db = {
-    mongoose,
-    User,
-    Favourite,
-    UpdatedPassword,
-    Item,
-    wishList,
-    LsrProduct,
-    LsrCategory,
-    PromoCodes,
-    ReserveOrder,
-    UserLogin,
-    UserActivity,
-    WebsiteViewed,
-    Subscribename,
-    Faq,
-    notifications,
-    Blogs,
-    Category
-};
 
-module.exports = db;
->>>>>>> origin/jiya_dev
+
+
+module.exports= db
+
+
+
